19b69a647af7e4b090012bfb4a2299179a9a1805
Mir Newaz <mir.newaz@checkatrade.com>
2022-12-21 16:48:17 +0000

    up it

---
+++ source/Dotnet.Extensions/Enum/EnumMemberExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet.Extensions.Enum
+{
+    public static class EnumMemberExtensions
+    {
+        /// <summary>
+        /// Returns EnumMember(Name) attribute of Enum value, if no EnumMember is set will return EnumValue.ToString()
+        /// </summary>
+        /// <param name="self">The instance</param>
+        /// <returns>The contents of the EnumMember attribute, or if that is not present, the enum.ToString</returns>
+        public static string EnumMemberName(this System.Enum self)
+            => self?
+                   .GetType()
+                   .GetField(self.ToString())
+                   ?.GetCustomAttribute<EnumMemberAttribute>()
+                   ?.Value
+               ?? self?.ToString() ?? string.Empty;
+
+        public static string? EnumMemberName(this FieldInfo? field)
+            => field
+                   ?.GetCustomAttributes(typeof(EnumMemberAttribute), false)
+                   ?.Cast<EnumMemberAttribute>()
+                   ?.FirstOrDefault()
+                   ?.Value
+               ?? field?.Name ?? string.Empty;
+    }
+}
---
+++ source/Dotnet.Extensions/Strings/ToTitleCaseExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Dotnet.Extensions.Strings
+{
+    public static class ToTitleCaseExtensions
+    {
+        /// <summary>
+        /// Converts the specified string to title case (except for words  that are entirely in uppercase, which are considered to be acronyms)
+        /// </summary>
+        /// <param name="self">The source string.</param>
+        /// <returns>The specified string converted to title case.</returns>
+        /// <example>var result = myString.ToTitleCase(); // turns "this string. like this." into "This String. Like This."</example>
+        /// <remarks>Note: This assumes any words in all caps to be acronyms, and does not change them. If this is not desired, consider calling .ToLower() first.</remarks>
+        public static string ToTitleCase(this string self)
+            => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(self ?? string.Empty);
+    }
+}
