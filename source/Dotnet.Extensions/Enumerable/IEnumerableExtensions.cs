e47b48462230d80255e4bb0bfb050ac2da18b9e3
developer-shah <mir.newaz.uk@gmail.com>
2024-05-19 14:22:56 +0100

    Add enumerable find extension method

---
+++ source/Dotnet.Extensions/Enumerable/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet.Extensions.Enumerable
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Find<T>(this IEnumerable<T> source, Func<T, bool> isMatch)
+        {
+            foreach (var item in source)
+            {
+                if (isMatch(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
